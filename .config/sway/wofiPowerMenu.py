0a18a28f65e81d4900e8f58bf260679c59f4e722
IgnaceMenace <74070019+IgnaceMenace@users.noreply.github.com>
2022-05-24 12:07:46 +0200

    Add files via upload

---
+++ .config/sway/wofiPowerMenu.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+
+def powerMenu():
+  keys = (
+    "\uf08b   Log Out",
+    "\uf023   Lock",
+    "\uf186   Suspend",
+    "\uf2dc   Hibernate",
+    "\uf021   Reboot",
+    "\uf011   Shutdown",
+  )
+
+  actions = (
+    "swaymsg exit",
+    "swaylock -C ~/.config/sway/swayLock.config",
+    "systemctl suspend",
+    "systemctl hibernate",
+    "systemctl reboot",
+    "systemctl poweroff"
+  )
+
+  options = "\n".join(keys)
+  choice = os.popen("echo -e '" + options + "' | wofi -d -i -p 'Power Menu' -W 200 -H 200 -k /dev/null").readline().strip()
+  if choice in keys:
+    os.popen(actions[keys.index(choice)])
+
+powerMenu()
