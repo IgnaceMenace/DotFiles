c70bbc6b6ec00e7bc28554c558e55bf4bb2da528
IgnaceMenace <74070019+IgnaceMenace@users.noreply.github.com>
2022-06-17 15:52:35 +0200

    Update setupFedW.sh

--- setupFedW.sh
+++ setupFedW.sh
@@ -14,6 +14,7 @@ pavucontrol
 wl-clipboard
 NetworkManager-tui
 light
+mc
 
 # Enable rpm Fusion (free and non-free)
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
