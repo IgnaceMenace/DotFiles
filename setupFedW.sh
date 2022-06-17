3a70a76c8f586d8423e6d6d117bd4f346b14d27f
IgnaceMenace <74070019+IgnaceMenace@users.noreply.github.com>
2022-06-17 11:27:23 +0200

    Update setupFedW.sh

--- setupFedW.sh
+++ setupFedW.sh
@@ -12,3 +12,9 @@ waybar
 wofi
 pavucontrol
 wl-clipboard
+NetworkManager-tui
+
+# Enable rpm Fusion (free and non-free)
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+# Install  the multimedia codecs 
+sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
