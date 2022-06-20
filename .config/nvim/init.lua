4e8fcc196f1f33248505d858bdf28a16bdf67cba
IgnaceMenace <74070019+IgnaceMenace@users.noreply.github.com>
2022-06-20 02:20:03 +0200

    Add files via upload

---
+++ .config/nvim/init.lua
@@ -0,0 +1,4 @@
+-- The whole setup is inspired by Chris@machine 
+-- the original source code can be found here https://github.com/LunarVim/Neovim-from-scratch
+require "neovugnace.options" -- vim options
+
