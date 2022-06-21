c4f81494c7dcb7e54b59d6e76ace7ddc172c57b5
IgnaceMenace <igparmentier@gmail.com>
2022-06-21 02:57:36 +0200

    remove old nvim

--- .config/nvim/init.lua
+++
@@ -1,4 +0,0 @@
--- The whole setup is inspired by Chris@machine 
--- the original source code can be found here https://github.com/LunarVim/Neovim-from-scratch
-require "neovugnace.options" -- vim options
-
