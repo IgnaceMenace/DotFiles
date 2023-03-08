09544545bfbf91ebc6839d61307f9173253bf81f
IgnaceMenace <74070019+IgnaceMenace@users.noreply.github.com>
2023-03-08 16:38:45 +0100

    Update fuzzelPowerMenu.py

--- .config/sway/fuzzelPowerMenu.py
+++ .config/sway/fuzzelPowerMenu.py
@@ -15,7 +15,7 @@ def powerMenu():
   actions = (
     "swaymsg exit",
     "swaylock -C ~/.config/sway/swayLock.config",
-    "systemctl suspend && swaylock -C ~/.config/sway/swayLock.config",
+    "systemctl suspend",
     "systemctl hibernate",
     "systemctl reboot",
     "systemctl poweroff"
@@ -23,7 +23,7 @@ def powerMenu():
 
   options = "\n".join(keys)
   #choice = os.popen("echo -e '" + options + "' | wofi -d -i -p 'Power Menu' -W 250 -H 230 -k /dev/null").readline().strip()
-  choice = os.popen("echo -e '" + options + "' | fuzzel -f 'JetBrainsMono NF' -C ebcb8bff -b 1d1f21ff -T footclient -B 3 -t 88c0d0ff -m a3be8cff -S d8dee9ff -s bf616aff -d -w 10 -l 6").readline().strip()
+  choice = os.popen("echo -e '" + options + "' | fuzzel -f 'JetBrainsMono NF' -C ebcb8bff -b 1d1f21ff -T footclient -B 3 -t 88c0d0ff -m a3be8cff -S d8dee9ff -s bf616aff -d -w 15 -l 6").readline().strip()
   if choice in keys:
     os.popen(actions[keys.index(choice)])
 
