c0df49b5d567b1edfc660cf3d9bd16234205777c
IgnaceMenace <74070019+IgnaceMenace@users.noreply.github.com>
2023-03-08 15:50:12 +0100

    Update setupFedW.sh

--- setupFedW.sh
+++ setupFedW.sh
@@ -165,6 +165,8 @@ sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-releas
 
 ## Multimedia codecs 
 sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel -y
+sudo dnf install lame\* --exclude=lame-devel -y
+sudo dnf group upgrade --with-optional Multimedia -y
 
 # DNF tweak
 sudo sed -i '9i\fastestmirror=true' /etc/dnf/dnf.conf
