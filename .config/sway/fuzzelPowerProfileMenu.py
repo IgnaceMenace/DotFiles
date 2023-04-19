742580badf94e018645452327905c4f79db035de
IgnaceMenace <74070019+IgnaceMenace@users.noreply.github.com>
2023-04-19 11:19:57 +0200

    Update fuzzelPowerProfileMenu.py

--- .config/sway/fuzzelPowerProfileMenu.py
+++ .config/sway/fuzzelPowerProfileMenu.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 import os
 
-def powerMenu():
-  
+def powerMenu(): 
   profiles = (
     "performance",
     "balanced",
@@ -16,7 +15,6 @@ def powerMenu():
   ]
   if currentProfile in profiles:
     keys[profiles.index(currentProfile)] = "" + keys[profiles.index(currentProfile)][1:]
-
   actions = (
     "powerprofilesctl set " + profiles[0],
     "powerprofilesctl set " + profiles[1],
@@ -26,7 +24,6 @@ def powerMenu():
   choice = os.popen("echo -e '" + options + "' | fuzzel -f 'JetBrainsMono NF'-13 -C ebcb8bff -b 1d1f21ff -T footclient -B 3 -t 88c0d0ff -m a3be8cff -S d8dee9ff -s bf616aff -d -w 13 -l 3").readline().strip()
   if choice in keys:
     os.popen(actions[keys.index(choice)])
-
 
 currentProfile = os.popen("powerprofilesctl get").readline().strip()
 powerMenu()
