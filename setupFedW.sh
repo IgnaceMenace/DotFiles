2bcc3296575ecc75596528a61168d1c8ac63ddf0
IgnaceMenace <74070019+IgnaceMenace@users.noreply.github.com>
2024-05-06 15:24:41 +0200

    Update setupFedW.sh

--- setupFedW.sh
+++ setupFedW.sh
@@ -150,6 +150,25 @@ sudo dnf install codium -y
 #sudo dnf install eid-mw -y
 #cd
 
+# Docker development toolset
+sudo dnf remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine \
+                  -y
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo groupadd docker
+sudo usermod -aG docker $USER
+# End Docker
+
 # Install JetBrains patched fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
 unzip JetBrainsMono.zip -d JetBrainsMono/
