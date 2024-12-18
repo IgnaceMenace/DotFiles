964a6af0c2a6ab8e02f5743c3bb9b33391afacd8
IgnaceMenace <74070019+IgnaceMenace@users.noreply.github.com>
2024-12-18 03:31:34 +0100

    Update setupFedW.sh

--- setupFedW.sh
+++ setupFedW.sh
@@ -115,6 +115,10 @@ sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
 sudo dnf install codium -y
 
+## Install extensions
+codium --install-extension ms-python.python
+codium --install-extension ms-toolsai.jupyter
+codium --install-extension ms-azuretools.vscode-docker
 # Install belgian eid software
 #Not always available when new version is out so better compile it
 #cd
